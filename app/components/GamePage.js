c69a66a848ddb5cc55430793ce719e72b18cc6b5
Arinde Eniola <eniolaarinde1@gmail.com>
2016-07-01 03:49:18 +0100

    complete the game page design amd make it responsive

--- app/components/GamePage.js
+++ app/components/GamePage.js
@@ -6,29 +6,33 @@ export default class GamePage extends Component {
 
   render() {
     return (
-      <div className="container">
-         <div className="row">
+      <div className="container-fluid">
+        <div className="row">
            <div className="header">
              <h1>Tic Tac Toe</h1>
            </div>
-         </div>
-         <div className="row">
-           <div className="col-md-6 col-md-offset-4 tictac-container">
-             <div className="row">
-               <div className="col-md-3 cell"><i className="fa fa-times font-times"></i></div>
-               <div className="col-md-3 cell"><i className="fa fa-circle-o font-circle"></i></div>
-               <div className="col-md-3 cell cell-right">d</div>
-               <div className="col-md-3 cell">s</div>
-               <div className="col-md-3 cell">d</div>
-               <div className="col-md-3 cell cell-right">d</div>
-               <div className="col-md-3 cell cell-bottom">s</div>
-               <div className="col-md-3 cell cell-bottom">d</div>
-               <div className="col-md-3 cell cell-right cell-bottom">d</div>
-             </div>
-           </div>
-         </div>
+        </div>
         <div className="row">
-          <div className="col-md-6 col-md-offset-4 info">
+          <div className="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-12 tictac-container">
+            <div className="row">
+              <div className="col-md-4 col-sm-4 col-xs-4 cell"><i className="fa fa-times font-times"></i></div>
+              <div className="col-md-4 col-sm-4 col-xs-4 cell"><i className="fa fa-circle-o font-circle"></i></div>
+              <div className="col-md-4 col-sm-4 col-xs-4 cell cell-right">d</div>
+            </div>
+            <div className="row">
+              <div className="col-md-4 col-sm-4 col-xs-4 cell">s</div>
+              <div className="col-md-4 col-sm-4 col-xs-4 cell">d</div>
+              <div className="col-md-4 col-sm-4 col-xs-4 cell cell-right">d</div>
+            </div>
+            <div className="row">
+              <div className="col-md-4 col-sm-4 col-xs-4 cell cell-bottom">s</div>
+              <div className="col-md-4 col-sm-4 col-xs-4 cell cell-bottom">d</div>
+              <div className="col-md-4 col-sm-4 col-xs-4 cell cell-right cell-bottom">d</div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="info">
             <span>Your turn to play</span>
           </div>
         </div>
