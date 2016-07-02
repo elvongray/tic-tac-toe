795e778679efc2791546bbaa6ec9f468fc6e8eab
Arinde Eniola <eniolaarinde1@gmail.com>
2016-07-02 19:29:48 +0100

    add the webpack prod config for the ghpage

---
+++ webpack.config.prod.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    path.resolve(__dirname, 'app/app.js')
+  ],
+
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      { test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader!postcss-loader'
+      }
+    ]
+  },
+
+  postcss: [ autoprefixer ],
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+    }),
+  ]
+}
